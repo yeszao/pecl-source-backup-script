31016474d30e0e30101ffd2477711a4dac5380bd
yeszao <yeszao@qq.com>
2019-09-23 12:03:27 +0800

    add schedule to push packages

--- run.py
+++ run.py
@@ -1,5 +1,29 @@
 from scrapy.cmdline import execute
+import schedule
+import time
+import subprocess
 
 
-#execute(['scrapy', 'crawl', 'pecl'])
-execute(['scrapy', 'crawl', 'php'])
+def pecl():
+    execute(['scrapy', 'crawl', 'pecl'])
+    push_pecl()
+
+
+def php():
+    execute(['scrapy', 'crawl', 'php'])
+
+
+def push_pecl():
+    subprocess.check_output(['git', '--git-dir=downloads/pecl/get/.git', 'add', '*'])
+    subprocess.check_output(['git', '--git-dir=downloads/pecl/get/.git', 'commit', '-m', 'update'])
+    subprocess.check_output(['git', '--git-dir=downloads/pecl/get/.git', 'pull', 'origin', 'gitee'])
+    subprocess.check_output(['git', '--git-dir=downloads/pecl/get/.git', 'pull', 'origin', 'github'])
+
+
+if __name__ == '__main__':
+    #schedule.every().day.at("2:30").do(pecl)
+    schedule.every(1).minutes.do(push_pecl)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
